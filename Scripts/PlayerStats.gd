e94fb8fdbd001008fe44264cd2638bb29bba1b9e
nathan-1206 <nathan1206@gmail.com>
2022-12-15 22:05:39 -0500

    added logos

--- Scripts/PlayerStats.gd
+++ Scripts/PlayerStats.gd
@@ -5,9 +5,6 @@ var personality = 4
 var efficiency = 4
 var skills = 4
 var hiredCompany = null
-var teacherMode = false
 
 func stats():
 	return [intelligence, personality, efficiency, skills]
-	
-
